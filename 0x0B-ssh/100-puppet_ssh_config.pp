dfa9980d1c62ef7776411b31827199b91c01637e
RayBar72 <3963@holbertonschool.com>
2022-04-19 16:00:33 -0300

    Task advanced. Fix indent

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # Setting up client ssh
 file_line { 'Using ssh key school':
   path =>  '/etc/ssh/ssh_config',
-  line =>  'IdentityFile ~/.shh/school',
+  line =>  '    IdentityFile ~/.shh/school',
 }
 file_line { 'Disable password authent':
   path =>  '/etc/ssh/ssh_config',
-  line =<  'PasswordAuthentication no',
+  line =<  '    PasswordAuthentication no',
 }
