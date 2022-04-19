a11d78fa6cdeac02ef4fc07cbcfb5990f760f241
RayBar72 <3963@holbertonschool.com>
2022-04-19 16:10:12 -0300

    Task advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 # Setting up client ssh
 file_line { 'Using ssh key school':
-  path =>  '/etc/ssh/ssh_config',
-  line =>  '    IdentityFile ~/.shh/school',
+  path    =>  '/etc/ssh/ssh_config',
+  line    =>  '  IdentityFile ~/.shh/school',
+  replace =>  true,
 }
 file_line { 'Disable password authent':
-  path =>  '/etc/ssh/ssh_config',
-  line =>  '    PasswordAuthentication no',
+  path    =>  '/etc/ssh/ssh_config',
+  line    =>  '  PasswordAuthentication no',
+  replace =>  true,
 }
