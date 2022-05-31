552774d87ba9c4b6227d2f73c782262382c82ff7
RayBar72 <3963@holbertonschool.com>
2022-05-31 12:35:26 -0300

    Backup 220531h1235

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Getting top 10 hot posts'''
+import requests
+
+
+def top_ten(subreddit):
+    '''Function that returns the top ten hot posts titles'''
+    red_url = 'http://reddit.com/r/{}/hot.json'
+    header = {'User-agent': 'Prueba:1-top_ten:v1'}
+    resp = requests.get(red_url.format(subreddit), headers=header)
+    inf = resp.json().get('data', {}).get('children', {})
+    if resp.status_code != 200:
+        return print('None')
+    for x in inf[0:10]:
+        print(x.get('data', {}).get('title'))
