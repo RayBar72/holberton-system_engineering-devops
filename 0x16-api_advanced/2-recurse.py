ca5a125917b06a8d0b0eec969ceea21b5435472f
RayBar72 <3963@holbertonschool.com>
2022-05-31 19:37:24 -0300

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,3 +3,27 @@
 Recursive function that queries the Reddit API and returns a
 list containing the titles of all hot articles for a given subreddit
 '''
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    '''Getting all posto of hot'''
+    red_url = 'http://reddit.com/r/{}/hot.json'
+    header = {'User-agent': 'Prueba:1-top_ten:v1'}
+    limit = {'limit': 100}
+    if isinstance(after, str):
+        if after != 'STOP':
+            limit['after'] = after
+        else:
+            return hot_list
+    resp = requests.get(red_url.format(subreddit), headers=header,
+                        params=limit)
+    if resp.status_code != 200:
+        return None
+    data = resp.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = 'STOP'
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return recurse(subreddit, hot_list, after)
