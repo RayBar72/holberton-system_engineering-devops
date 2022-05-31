06450f2273259208427e0461ff1705d169cde467
RayBar72 <3963@holbertonschool.com>
2022-05-31 14:13:08 -0300

    Backup 220531

---
+++ 0x16-api_advanced/User.mm
@@ -0,0 +1,105 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="User" FOLDED="false" ID="ID_1701092769" CREATED="1653998195149" MODIFIED="1653998211981" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Datos Administrativos" POSITION="right" ID="ID_400065721" CREATED="1653998259644" MODIFIED="1653998289421">
+<edge COLOR="#ff0000"/>
+<node TEXT="ID Usuario" ID="ID_596042240" CREATED="1653998517104" MODIFIED="1653998616848"/>
+<node TEXT="Contrase&#xf1;a" ID="ID_1833820768" CREATED="1653998522529" MODIFIED="1653998538880"/>
+<node TEXT="Nombre" ID="ID_263050762" CREATED="1653998539231" MODIFIED="1653998547192"/>
+<node TEXT="Correo electr&#xf3;nico" ID="ID_159788542" CREATED="1653998547509" MODIFIED="1653998581957"/>
+</node>
+<node TEXT="Gasolineras" POSITION="right" ID="ID_1999098496" CREATED="1653998290355" MODIFIED="1653998306407">
+<edge COLOR="#0000ff"/>
+<node TEXT="ID Gasolinera" ID="ID_1313976879" CREATED="1653999159708" MODIFIED="1653999171408"/>
+<node TEXT="ID Del Usuario" ID="ID_582612178" CREATED="1653999172180" MODIFIED="1653999180589"/>
+<node TEXT="Nombre" ID="ID_1804559881" CREATED="1654000338150" MODIFIED="1654000343946"/>
+<node TEXT="Ubicaci&#xf3;n de la gasolinera" ID="ID_915399556" CREATED="1653999181841" MODIFIED="1653999264222"/>
+<node TEXT="Surtidor" ID="ID_1512052782" CREATED="1654000359608" MODIFIED="1654015765637">
+<node TEXT="ID Estaciones" ID="ID_253134700" CREATED="1654000374010" MODIFIED="1654000383975"/>
+<node TEXT="ID Gasolinera" ID="ID_707591359" CREATED="1654000385455" MODIFIED="1654000398688"/>
+<node TEXT="ID Del Usuario" ID="ID_1981405528" CREATED="1654000399700" MODIFIED="1654000406534"/>
+<node TEXT="Nombre" ID="ID_669963966" CREATED="1654001100536" MODIFIED="1654001109065"/>
+<node TEXT="Wallet" ID="ID_754662070" CREATED="1654001110717" MODIFIED="1654001131084"/>
+<node TEXT="Transacciones" ID="ID_1175275556" CREATED="1654001228007" MODIFIED="1654001235483">
+<node TEXT="ID Estaciones" ID="ID_1614998330" CREATED="1654000374010" MODIFIED="1654000383975"/>
+<node TEXT="ID Gasolinera" ID="ID_268260998" CREATED="1654000385455" MODIFIED="1654000398688"/>
+<node TEXT="ID Del Usuario" ID="ID_1663316475" CREATED="1654000399700" MODIFIED="1654000406534"/>
+<node TEXT="ID De la Transacci&#xf3;n" ID="ID_864441238" CREATED="1654001248133" MODIFIED="1654001259357"/>
+<node TEXT="Monto Pesos" ID="ID_627808296" CREATED="1654001264040" MODIFIED="1654001273321"/>
+<node TEXT="Fee" ID="ID_1571482966" CREATED="1654001287889" MODIFIED="1654001560165"/>
+<node TEXT="Tipo de Cambio" ID="ID_521244659" CREATED="1654001273961" MODIFIED="1654001286023"/>
+</node>
+</node>
+</node>
+<node TEXT="M&#xe9;todos" POSITION="right" ID="ID_1412990088" CREATED="1653998308966" MODIFIED="1653998329641">
+<edge COLOR="#00ff00"/>
+<node TEXT="Crear" ID="ID_1583847879" CREATED="1653998469147" MODIFIED="1653998475100"/>
+<node TEXT="Actualizar datos" ID="ID_806183244" CREATED="1653998476251" MODIFIED="1653998490782"/>
+<node TEXT="Borrar" ID="ID_212256034" CREATED="1653998491518" MODIFIED="1653998499137"/>
+<node TEXT="Obtener JSON" ID="ID_1180289147" CREATED="1653998500152" MODIFIED="1653998508102"/>
+</node>
+</node>
+</map>
