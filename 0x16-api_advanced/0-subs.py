7b48f4dcff79d33310f982399cd9bc9e3451fb58
RayBar72 <3963@holbertonschool.com>
2022-05-31 11:32:38 -0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''Getting number of subscribers for a subreddit'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Function that returns the number of users'''
+    red_url = 'http://reddit.com/r/{}/about.json'
+    header = {'User-agent': 'Prueba:0-subs:v1'}
+    resp = requests.get(red_url.format(subreddit), headers=header)
+    if resp.status_code != 200:
+        return 0
+    return resp.json().get('data', {}).get('subscribers', 0)
